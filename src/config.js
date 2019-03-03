ff1dfd9ec54e3edf079778ed4d379402ecee6a6d
Shawn Rivers <kaushou@gmail.com>
2019-03-03 15:38:24 +0900

    Re-prettier codes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key: "5M94fgNYJGWKmQEAJiAVlxyd1",
-  consumer_secret: "pbvUWwHLLOuF957m1dyf99hi3uNkSl3ARjSWqHOs7YQFm4bu1k",
-  access_token: "1042795881752276993-3jDfNktrmtdQa7PXf4vD0Ycd2EJjwY",
-  access_token_secret: "TrFzOvpKjThKLKNo4KuvlQEfF0O107ll9GEuR4NwhrAwV",
+  consumer_key: '5M94fgNYJGWKmQEAJiAVlxyd1',
+  consumer_secret: 'pbvUWwHLLOuF957m1dyf99hi3uNkSl3ARjSWqHOs7YQFm4bu1k',
+  access_token: '1042795881752276993-3jDfNktrmtdQa7PXf4vD0Ycd2EJjwY',
+  access_token_secret: 'TrFzOvpKjThKLKNo4KuvlQEfF0O107ll9GEuR4NwhrAwV',
   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  strictSSL: true // optional - requires SSL certificates to be valid.
+  strictSSL: true, // optional - requires SSL certificates to be valid.
 };
--- src/temp/getid.js
+++ src/temp/getid.js
@@ -1,19 +1,17 @@
-const Twit = require("twit");
-const config = require("../config");
+const Twit = require('twit');
+const config = require('../config');
 
 const T = new Twit(config);
 
-const accountName = "nikkansports";
+const accountName = 'nikkansports';
 
 T.get(
-  "statuses/user_timeline",
+  'statuses/user_timeline',
   {
     screen_name: accountName,
-    count: 1
+    count: 1,
   },
   (err, data, response) => {
-    console.log(
-      data[0].user.name + "(@" + data[0].user.screen_name + "): " + data[0].user.id_str
-    );
-  }
+    console.log(data[0].user.name + '(@' + data[0].user.screen_name + '): ' + data[0].user.id_str);
+  },
 );
