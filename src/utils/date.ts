dcce11a1b78836701b8f8ab3c0fcd11333ad2e6b
Shawn Rivers <kaushou@gmail.com>
2019-03-08 02:45:18 +0900

    make format data function available to receive params from outside

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,10 +1,26 @@
-const now = new Date();
-const tokyoDate = now.toLocaleString('en-US', {
-  timeZone: 'Asia/Tokyo',
-  year: 'numeric',
-  month: '2-digit',
-  day: '2-digit',
-});
+export type IDate = {
+  year: string;
+  month: string;
+  day: string;
+};
 
-export const [month, day, year] = tokyoDate.split('/');
+export const getToday = (): IDate => {
+  const now = new Date();
+  const tokyoDate = now.toLocaleString('en-US', {
+    timeZone: 'Asia/Tokyo',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
 
+  const [month, day, year] = tokyoDate.split('/');
+  return { year, month, day };
+};
+
+export const getOneDigitDate = (date: string): string => {
+  if (date[0] === '0') {
+    return date.slice(1);
+  } else {
+    return date;
+  }
+};
