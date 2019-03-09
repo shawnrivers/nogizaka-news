a259821f6bead9b7e7b475c98dda111c281e966f
Shawn Rivers <kaushou@gmail.com>
2019-03-09 16:45:48 +0900

    Rename Twit config object name

--- src/utils/twit.ts
+++ src/utils/twit.ts
@@ -1,6 +1,6 @@
 import * as Twit from 'twit';
 
-const config = {
+const twitConfig = {
   consumer_key: '5M94fgNYJGWKmQEAJiAVlxyd1',
   consumer_secret: 'pbvUWwHLLOuF957m1dyf99hi3uNkSl3ARjSWqHOs7YQFm4bu1k',
   access_token: '1042795881752276993-3jDfNktrmtdQa7PXf4vD0Ycd2EJjwY',
@@ -9,4 +9,4 @@ const config = {
   strictSSL: true, // optional - requires SSL certificates to be valid.
 };
 
-export const T = new Twit(config);
+export const T = new Twit(twitConfig);
