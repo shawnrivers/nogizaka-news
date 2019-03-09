9ef6651f7be84b2e133992d6727774fb4bf15a42
Shawn Rivers <kaushou@gmail.com>
2019-03-09 16:54:50 +0900

    Add a function to find and format schedules based on the given HTML

--- src/actions/nogizaka.ts
+++ src/actions/nogizaka.ts
@@ -1,3 +1,6 @@
+import * as cheerio from 'cheerio';
+import { scheduleTypes } from '../utils/constants';
+import { ITypeSchedules } from '../utils/types';
 import { nogizakaNames } from '../utils/constants';
 
 export const relatesToNogizaka = (text: string): boolean => {
@@ -12,3 +15,34 @@ export const relatesToNogizaka = (text: string): boolean => {
     return false;
   }
 };
+
+const getDaySchedules = (html: any, day: string): ITypeSchedules[] => {
+  let schedules: ITypeSchedules[] = [];
+
+  const $ = cheerio.load(html);
+  const dayElement = $(`#d${day}`);
+
+  for (const scheduleType of scheduleTypes) {
+    let typeSchedulesData = [];
+    const typeSchedulesElement = dayElement.find(scheduleType.className).get();
+
+    if (typeSchedulesElement.length !== 0) {
+      for (const typeScheduleElement of typeSchedulesElement) {
+        const scheduleData = typeScheduleElement.children[0].data;
+        typeSchedulesData.push(scheduleData);
+      }
+    }
+
+    const typeSchedules = {
+      type: {
+        name: scheduleType.type,
+        displayName: scheduleType.displayName,
+      },
+      data: typeSchedulesData,
+    };
+    schedules.push(typeSchedules);
+  }
+
+  return schedules;
+};
+
