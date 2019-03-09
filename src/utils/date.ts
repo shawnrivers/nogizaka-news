4b7fa98f9c659c9904a30dd768d1df0e2f1d393a
Shawn Rivers <kaushou@gmail.com>
2019-03-09 16:48:30 +0900

    Add function to get the full Tokyo date string

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -17,6 +17,22 @@ export const getToday = (): IDate => {
   return { year, month, day };
 };
 
+export const getCurrentFullDate = (): string => {
+  const now = new Date();
+  const currentFullDate = now.toLocaleString('en-US', {
+    timeZone: 'Asia/Tokyo',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  return currentFullDate;
+};
+
 export const getOneDigitDate = (date: string): string => {
   if (date[0] === '0') {
     return date.slice(1);
