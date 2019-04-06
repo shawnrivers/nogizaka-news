d939244126383beb0db35fc46463b6e9d7ef32c2
Shawn Rivers <kaushou@gmail.com>
2019-04-07 00:09:35 +0900

    Remove tests

--- src/actions/nogizaka.test.ts
+++
@@ -1,5 +0,0 @@
-import { relatesToNogizaka } from "./nogizaka";
-
-test('relatesToNogizaka should return true when the parameter string contains "乃木坂46"', () => {
-  expect(relatesToNogizaka('乃木坂46')).toBe(true);
-})
