7343e5b12d8999e91e647c314fbec4cb60fc2490
Shawn Rivers <kaushou@gmail.com>
2019-04-06 23:32:23 +0900

    Initialize global last tweets before starting retweeting

--- src/app.ts
+++ src/app.ts
@@ -2,29 +2,32 @@ import { retweetNogizakaRelated } from './actions/retweetNews';
 import { tweetTodaySchedules } from './actions/tweetSchedules';
 import { NOGIZAKA_RELATED_ACCOUNTS, NEWS_MEDIA_ACCOUNTS, SHOWROOM_ACCOUNT } from './utils/constants';
 import { getMillisecondsTilTomorrowAt, getCurrentFullDate } from './utils/date';
+import { getInitialLastTweets } from './utils/lastTweets';
 
 const watchAndRetweet = (interval: number) => {
+  global.lastTweets = getInitialLastTweets();
+
   retweetNogizakaRelated(NOGIZAKA_RELATED_ACCOUNTS, NEWS_MEDIA_ACCOUNTS, SHOWROOM_ACCOUNT);
   setInterval(() => retweetNogizakaRelated(NOGIZAKA_RELATED_ACCOUNTS, NEWS_MEDIA_ACCOUNTS, SHOWROOM_ACCOUNT), interval);
 };
 
 const scheduleTweet = (hour: number) => {
   let nextTweetTimeout = getMillisecondsTilTomorrowAt(hour);
-  
+
   console.log(`[Schedules] Tomorrow's schedules will be tweeted after ${nextTweetTimeout / 1000} sec\n`);
-  
+
   const timeoutTweet = async () => {
     console.log("[Schedules] Started tweeting today's schedules.");
-    
+
     await tweetTodaySchedules();
-    
+
     console.log("[Schedules] Today's schedules tweeting finished at Tokyo time:", getCurrentFullDate());
-    
+
     nextTweetTimeout = getMillisecondsTilTomorrowAt(hour);
     console.log(`[Schedules] Tomorrow's schedules will be tweeted after ${nextTweetTimeout / 1000} sec\n`);
     setTimeout(timeoutTweet, nextTweetTimeout);
   };
-  
+
   setTimeout(() => timeoutTweet(), nextTweetTimeout);
 };
 
