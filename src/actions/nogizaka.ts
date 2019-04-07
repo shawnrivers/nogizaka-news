019d58b570e3b2fda31ed40e4debf779a2b22eea
Shawn Rivers <kaushou@gmail.com>
2019-04-07 15:26:10 +0900

    Add graduates schedules scrapper

--- src/actions/nogizaka.ts
+++ src/actions/nogizaka.ts
@@ -54,7 +54,7 @@ const getDaySchedules = (html: any, day: string): ITypeSchedules[] => {
   return schedules;
 };
 
-export const getSchedules = async (date: IDate): Promise<ITypeSchedules[]> => {
+export const getNogizakaSchedules = async (date: IDate): Promise<ITypeSchedules[]> => {
   let schedules: ITypeSchedules[] = [];
 
   const url = `http://www.nogizaka46.com/schedule/?to=${date.year}${date.month}`;
@@ -68,4 +68,91 @@ export const getSchedules = async (date: IDate): Promise<ITypeSchedules[]> => {
     });
 
   return schedules;
+};
+
+export const getGraduatesSchedules = async (date: IDate): Promise<ITypeSchedules[]> => {
+  const { year, month, day } = date;
+
+  const urlNishino = {
+    memberName: '西野七瀬',
+    url: `https://nishinonanase.com/s/m04/media/list?ima=2551&dy=${year}${month}`,
+  };
+  const urlIkoma = {
+    memberName: '生駒里奈',
+    url: `https://ikomarina.com/s/m01/media/list?ima=2925&dy=${year}${month}`,
+  };
+  const urlWakatsuki = {
+    memberName: '若月佑美',
+    url: `https://wakatsukiyumi.jp/s/m05/media/list?ima=2814&dy=${year}${month}`,
+  };
+  const urlObjects = [urlNishino, urlIkoma, urlWakatsuki];
+
+  let normalizedSchedules: ITypeSchedules[] = [];
+  let rawSchedules: ITypeSchedule[] = [];
+
+  for (const urlObj of urlObjects) {
+    const { memberName } = urlObj;
+
+    let memberSchedules: ITypeSchedule[] = [];
+
+    try {
+      const html = await req(urlObj.url);
+      const $ = cheerio.load(html);
+
+      const dateDay = year + month + day;
+      const dayElements = $(`[data-day=${dateDay}]`);
+      const dayElement = dayElements.find('.list_card');
+
+      dayElement.map((index, element) => {
+        const type = $(element)
+          .find('.category')
+          .text();
+        const date = $(element)
+          .find('.date')
+          .text();
+        const title = $(element)
+          .find('.title')
+          .text();
+
+        memberSchedules.push({
+          type,
+          schedule: { date, title, memberName },
+        });
+      });
+
+      rawSchedules.push(...memberSchedules);
+    } catch (err) {
+      console.log('Error:', err);
+    }
+  }
+
+  for (let i = 0; i < GRADUATES_SCHEDULE_TYPE_LIST.length; i++) {
+    const scheduleType = GRADUATES_SCHEDULE_TYPE_LIST[i];
+
+    normalizedSchedules.push({
+      type: scheduleType,
+      data: [],
+    });
+
+    let schedulesForSorting = [];
+
+    for (const rawSchedule of rawSchedules) {
+      if (rawSchedule.type === scheduleType) {
+        schedulesForSorting.push(rawSchedule.schedule);
+      }
+    }
+
+    schedulesForSorting.sort((scheduleA, scheduleB) => compareDates(scheduleA.date, scheduleB.date));
+
+    for (const scheduleForSorting of schedulesForSorting) {
+      const scheduleText =
+        scheduleForSorting.date !== ''
+          ? `${scheduleForSorting.date} ${scheduleForSorting.title} ${scheduleForSorting.memberName}`
+          : `${scheduleForSorting.title} ${scheduleForSorting.memberName}`;
+
+      normalizedSchedules[i].data.push(scheduleText);
+    }
+  }
+
+  return normalizedSchedules;
 };
