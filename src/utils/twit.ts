da251499ecedc34bcb88db921f000bceefc3ac15
Shawn Rivers <kaushou@gmail.com>
2019-04-11 22:47:01 +0900

    Use environmental variables instead of plain text

--- src/utils/twit.ts
+++ src/utils/twit.ts
@@ -1,15 +1,10 @@
 import * as Twit from 'twit';
 
-console.log('CONSUMER_KEY:', process.env.CONSUMER_KEY);
-console.log('CONSUMER_SECRET:', process.env.CONSUMER_SECRET);
-console.log('ACCESS_TOKEN:', process.env.ACCESS_TOKEN);
-console.log('ACCESS_TOKEN_SECRET:', process.env.ACCESS_TOKEN_SECRET);
-
 export const T = new Twit({
-  consumer_key: '5M94fgNYJGWKmQEAJiAVlxyd1',
-  consumer_secret: 'pbvUWwHLLOuF957m1dyf99hi3uNkSl3ARjSWqHOs7YQFm4bu1k',
-  access_token: '1042795881752276993-3jDfNktrmtdQa7PXf4vD0Ycd2EJjwY',
-  access_token_secret: 'TrFzOvpKjThKLKNo4KuvlQEfF0O107ll9GEuR4NwhrAwV',
+  consumer_key: process.env.CONSUMER_KEY ? process.env.CONSUMER_KEY : '',
+  consumer_secret: process.env.CONSUMER_SECRET ? process.env.CONSUMER_SECRET : '',
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000,
   strictSSL: true,
 });
