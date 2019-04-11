d44b7dd0401d4e0b022d5e08f63f51ee13ef05d4
Shawn Rivers <kaushou@gmail.com>
2019-04-12 02:29:03 +0900

    Simplify conditionals

--- src/utils/twit.ts
+++ src/utils/twit.ts
@@ -1,8 +1,8 @@
 import * as Twit from 'twit';
 
 export const T = new Twit({
-  consumer_key: process.env.CONSUMER_KEY ? process.env.CONSUMER_KEY : '',
-  consumer_secret: process.env.CONSUMER_SECRET ? process.env.CONSUMER_SECRET : '',
+  consumer_key: process.env.CONSUMER_KEY || '',
+  consumer_secret: process.env.CONSUMER_SECRET || '',
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000,
