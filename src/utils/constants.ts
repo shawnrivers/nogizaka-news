1eed50655f7fe60d95ac9d9ff1fcf0183e49554a
Shawn Rivers <kaushou@gmail.com>
2019-09-29 00:32:28 +0900

    Add Sakurai listener

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -20,6 +20,7 @@ export const SHOWROOM_ACCOUNT: IWatchedAccount = { id: '2212674829', count: 30 }
 export const NOGIZAKA_NAMES = [
   '乃木坂46',
   'nogizaka46',
+  '桜井玲香',
   '斉藤優里',
   '伊藤かりん',
   '衛藤美彩',
