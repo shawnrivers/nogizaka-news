7ba1d4e66728709ea88e0f9e552935b547670a93
Shawn Rivers <kaushou@gmail.com>
2020-05-02 12:22:58 +0900

    Create TweetFetcher

---
+++ src/actors/fechers/TweetFetcher/index.ts
@@ -0,0 +1,89 @@
+import * as Twit from 'twit';
+import { arrayToObject } from '../../../utils/array';
+
+type WatchingAccount = {
+  id: string;
+  count: number;
+};
+
+type GetTweetResponse = Omit<Twit.PromiseResponse, 'data'> & {
+  data: {
+    id_str: string;
+    created_at: string;
+    user: {
+      id_str: string;
+      screen_name: string;
+    };
+    text: string;
+  }[];
+};
+
+type Tweet = {
+  id: string;
+  createdDate: Date;
+  userId: string;
+  userName: string;
+  text: string;
+};
+
+export class TweetFetcher {
+  twitter: Twit;
+  watchingAccounts: WatchingAccount[];
+  lastTweets: { [key: string]: { tweetId: string | null } };
+
+  constructor({ twitter, watchingAccounts }: { twitter: Twit; watchingAccounts: WatchingAccount[] }) {
+    this.twitter = twitter;
+    this.watchingAccounts = watchingAccounts;
+    this.lastTweets = arrayToObject(
+      watchingAccounts.map((account) => ({
+        id: account.id,
+        tweetId: null,
+      })),
+      'id',
+    );
+  }
+
+  public async getTweets(): Promise<Tweet[]> {
+    const tweets = [];
+
+    for (const watchingAccount of this.watchingAccounts) {
+      const accountTweets = await this.getTimeline(watchingAccount);
+      tweets.push(...accountTweets);
+    }
+
+    return tweets;
+  }
+
+  private async getTimeline(account: WatchingAccount): Promise<Tweet[]> {
+    const timeline = [];
+
+    try {
+      const response = (await this.twitter.get('statuses/user_timeline', {
+        user_id: account.id,
+        include_rts: false,
+        count: account.count,
+        since_id: this.lastTweets[account.id].tweetId ?? undefined,
+      })) as GetTweetResponse;
+
+      for (const data of response.data) {
+        timeline.push({
+          id: data.id_str,
+          createdDate: new Date(data.created_at),
+          userId: data.user.id_str,
+          userName: data.user.screen_name,
+          text: data.text,
+        });
+      }
+
+      timeline.sort((tweetA, tweetB) => (tweetA.createdDate > tweetB.createdDate ? 1 : -1));
+
+      if (timeline.length > 0) {
+        this.lastTweets[account.id].tweetId = timeline.slice(-1)[0].id;
+      }
+    } catch (error) {
+      console.log('Get timeline error:', error);
+    }
+
+    return timeline;
+  }
+}
---
+++ src/utils/array.ts
@@ -0,0 +1,2 @@
+export const arrayToObject = <T>(array: T[], keyField: keyof T): { [key: string]: T } =>
+  Object.assign({}, ...array.map((item) => ({ [String(item[keyField])]: item })));
