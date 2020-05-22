d0ba4cb4faf353130f6dadfd91eb0b7fa89bd4ce
Shawn Rivers <kaushou@gmail.com>
2020-05-22 14:59:06 +0900

    Add type property to watching accounts

--- src/actors/fechers/TweetFetcher/types.d.ts
+++ src/actors/fechers/TweetFetcher/types.d.ts
@@ -1,8 +1,12 @@
 import * as Twit from 'twit';
+import { AccountId } from '../../../constants/accounts';
+
+export type AccountType = 'nogizaka' | 'news' | 'showroom' | 'member';
 
 export type WatchingAccount = {
-  id: string;
+  id: AccountId;
   count: number;
+  type: AccountType;
 };
 
 export type GetTweetResponse = Omit<Twit.PromiseResponse, 'data'> & {
