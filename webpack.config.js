b6656f9802b83a5efe3316e9e5f32bfaf09db0cd
Shawn Rivers <kaushou@gmail.com>
2020-06-07 21:37:16 +0900

    Use babel for compiling TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   target: 'node',
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.ts?$/,
-        use: 'ts-loader',
+        test: /\.ts|js?$/,
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
   },
 };
